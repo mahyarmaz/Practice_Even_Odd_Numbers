5992627ead1cb4b95937e1ec1666cdb689da7387
mahyarmaz <52723120+mahyarmaz@users.noreply.github.com>
2019-07-18 19:47:27 -0400

    Add files via upload
    Tells a number is Even or Odd

---
+++ Even or Odd.py
@@ -0,0 +1,14 @@
+i = 1
+while i <= 3:
+    number = int(input('Please enter a number: '))
+    i = i + 1
+    if number == 0:
+        print('Zero is Even')
+    elif number % 2 == 0:
+        print('Your number is Even :)')
+    else:
+        print('Your number is Odd :)')
+
+
+
+
